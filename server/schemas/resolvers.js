db2c372fd2aeac20765b88e500d6e529dd13f35d
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-11 19:58:57 +0930

    Added login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,6 +18,23 @@ const resolvers = {
            const user = await User.create({ username, email, password });
            const token = signToken(user);
            return { token, user }; 
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect email or password, please try again');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect email or password, please try again');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
         }
     }
 }
