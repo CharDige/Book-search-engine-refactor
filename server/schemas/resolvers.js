751604dda92c8f2c377d97c485bc5c833cc37e75
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-08-11 19:47:41 +0930

    Added addUser mutation to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
+        // Find a single user
         me: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({ _id: context.user._id });
@@ -11,4 +12,12 @@ const resolvers = {
             throw new AuthenticationError("Can't find user");
         },
     },
+    Mutation: {
+        // Add a user
+        addUser: async (parent, { username, email, password }) => {
+           const user = await User.create({ username, email, password });
+           const token = signToken(user);
+           return { token, user }; 
+        }
+    }
 }
